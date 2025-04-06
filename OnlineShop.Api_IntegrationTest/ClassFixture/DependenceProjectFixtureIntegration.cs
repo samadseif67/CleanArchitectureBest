f96fbb677c7207a20aa30a453f568c2f42dece65
samad <samadseif67@gmail.com>
2025-04-06 21:25:06 +0330

    1404-01-17 2125

---
+++ OnlineShop.Api_IntegrationTest/ClassFixture/DependenceProjectFixtureIntegration.cs
@@ -0,0 +1,88 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using OnlineShop.Api.Controllers;
+using OnlineShop.Domain.Entities;
+using OnlineShop.Domain.IPersistence;
+using OnlineShop.Persistence.Context;
+using OnlineShop.Persistence.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Tynamix.ObjectFiller;
+
+namespace OnlineShop.Api_IntegrationTest.ClassFixture
+{
+    public class DependenceProjectFixtureIntegration : IDisposable
+    {
+        public OnlineShopDbContext _dbContext;
+        public IMediator _mediator { get; set; }
+        public CategoryController _categoryController { get; set; }
+        public ICategoryRepository _categoryRepository;
+
+
+        public DependenceProjectFixtureIntegration(params Assembly[] handlerAssembly)
+        {
+
+            string OnlineShopTestConnection = "Server=DESKTOP-6OT3NPD\\SQL2022;Database=OnlineShop1404Test;User Id=sa;password=123456;Trusted_Connection=False;MultipleActiveResultSets=true;";
+            var options = new DbContextOptionsBuilder<OnlineShopDbContext>().UseSqlServer(OnlineShopTestConnection).Options;
+
+            _dbContext = new OnlineShopDbContext(options);
+            //********************************************************************
+
+            _dbContext.Database.EnsureCreated();
+
+            //*******************************************************************
+
+
+
+            _categoryRepository = new CategoryRepository(_dbContext);
+
+
+            SeedDatabase();
+            var services = new ServiceCollection();
+
+
+            services.AddMediatR(handlerAssembly);
+            //services.AddMediatR((typeof(GetAllCategoryHandler).Assembly));
+            //services.AddMediatR(FindAssembliesWithHandlers());
+
+            services.AddScoped(_ => _dbContext);
+            services.AddScoped(_ => _categoryRepository);
+
+
+            var serviceProvider = services.BuildServiceProvider();
+            _mediator = serviceProvider.GetRequiredService<IMediator>();
+
+
+
+
+
+            _categoryController = new CategoryController(_mediator);
+
+
+
+        }
+
+        private void SeedDatabase()
+        {
+            // مقداردهی اولیه دیتابیس
+            var hasData = _dbContext.Categories.Any(x => x.IsDelete == false);
+            if (!hasData)
+            {
+                var m = new Filler<Category>().Create(10);
+                _dbContext.Categories.AddRange(m);
+                _dbContext.SaveChanges();
+            }
+
+        }
+
+        public void Dispose()
+        {
+            _dbContext.Database.EnsureDeleted();
+        }
+    }
+}
---
+++ OnlineShop.Api_IntegrationTest/OnlineShopApi/Controllers/CategoryControllerIntegrationTest.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Api_IntegrationTest.ClassFixture;
+using OnlineShop.Application.Dto;
+using OnlineShop.Application.Feature.CategoryBusiness.Handler.Command;
+using OnlineShop.Application.Feature.CategoryBusiness.Handler.Query;
+using OnlineShop.Application.ValidationDto;
+using OnlineShop.Common.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tynamix.ObjectFiller;
+
+namespace OnlineShop.Api_IntegrationTest.OnlineShopApi.Controllers
+{
+    public class CategoryControllerIntegrationTest
+    {
+        public readonly DependenceProjectFixtureIntegration fixture;
+        public CategoryControllerIntegrationTest()
+        {
+            fixture = new DependenceProjectFixtureIntegration(
+                typeof(GetAllCategoryHandler).Assembly,
+                typeof(GetCategoryHandler).Assembly,
+                typeof(CreateCategoryHandler).Assembly
+                );
+        }
+
+
+        [Fact]
+        public async void GetAllCategory_ComponentTest()
+        {
+
+            var result = await fixture._categoryController.GetAllCategory();
+            var okResult = result as OkObjectResult;
+            var lstCategoryDto = okResult.Value as List<CategoryDto>;
+
+
+            Assert.IsType<List<CategoryDto>>(lstCategoryDto);
+            Assert.NotNull(result);
+            Assert.NotEmpty(lstCategoryDto);
+        }
+
+        [Fact]
+        public async void GetCategory_ComponentTest()
+        {
+            var find = fixture._dbContext.Categories.FirstOrDefault();
+            Guid categoryId = find.ID;
+
+            var result = await fixture._categoryController.GetCategory(categoryId);
+            var okResult = result as OkObjectResult;
+            var lstCategoryDto = okResult.Value as CategoryDto;
+
+
+            Assert.IsType<CategoryDto>(lstCategoryDto);
+            Assert.NotNull(result);
+
+        }
+
+        [Fact]
+        public async void AddCategory_ComponentTest()
+        {
+            //Arrange
+            CategoryDto categoryDto = new Filler<CategoryDto>().Create();
+            categoryDto.ID = Guid.Empty;
+
+            //Act
+            var result = await fixture._categoryController.AddCategory(categoryDto);
+            var okResult = result as OkObjectResult;
+            Result<CategoryDto> result1 = okResult.Value as Result<CategoryDto>;
+
+            Guid categoryId = result1.Data.ID;
+            var find = fixture._dbContext.Categories.FirstOrDefault(x => x.IsDelete == false && x.ID == categoryId);
+
+
+            //اعتبار سنجی داده ها
+            var validator = new CategoryDtoValidator();
+            var resultValidator = validator.Validate(categoryDto);
+
+
+
+
+            //Assert
+            Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(categoryDto.Title, find.Title);
+            Assert.Equal(categoryDto.Description, find.Description);
+            Assert.True(resultValidator.IsValid);
+
+        }
+
+
+
+    }
+}
