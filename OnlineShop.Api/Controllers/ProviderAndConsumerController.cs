4cf4830d003e4fab9cc61f353877cf0c2fdb1a1d
samad <samadseif67@gmail.com>
2025-04-11 23:59:38 +0330

    1404-01-22

---
+++ OnlineShop.Api/Controllers/ProviderAndConsumerController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShop.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProviderAndConsumerController : ControllerBase
+    {
+       
+
+    }
+}
---
+++ OrderAppConsumer/Service/VerifyrPoductService.cs
@@ -0,0 +1,86 @@
+﻿
+
+
+
+
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace OrderAppConsumer.Service
+{
+    public interface IVerifyrPoductService
+    {
+        VerifyProductDto Verify(ProductDto productDto);
+    }
+    public class VerifyrPoductService : IVerifyrPoductService
+    {
+
+
+        private readonly IRestClient restClient;
+        public VerifyrPoductService(IRestClient restClient)
+        {
+            this.restClient = restClient;
+        }
+
+        public VerifyProductDto Verify(ProductDto productDto)
+        {
+            var request = new RestRequest($"/api/product/verify/{productDto.id}",Method.Get);
+             RestResponse restResponse=  restClient.Execute(request);
+             var productRemote= JsonConvert.DeserializeObject<ProductVerifyOnServerProductDto>(restResponse.Content);
+
+            return Verify(productDto, productRemote);
+        }
+
+
+
+        private VerifyProductDto Verify(ProductDto local, ProductVerifyOnServerProductDto remote)
+        {
+            if(local.Name==remote.Name)
+            {
+                return new VerifyProductDto
+                {
+                    IsCorrect = true
+                };
+            }
+            else
+            {
+                return new VerifyProductDto
+                {
+                    Name = remote.Name,
+                    IsCorrect=false
+                };
+            }
+        }
+
+
+    }
+
+
+
+ 
+
+
+
+    public class VerifyProductDto
+    {
+        public string Name { get; set; }
+        public bool IsCorrect { get; set; }
+    }
+
+    public class ProductVerifyOnServerProductDto
+    {
+        public Guid id { get; set; }
+        public string Name { get; set; }
+    }
+
+
+
+
+    public class ProductDto
+    {
+        public Guid id { get; set; }
+        public string Name { get; set; }
+    }
+
+
+}
