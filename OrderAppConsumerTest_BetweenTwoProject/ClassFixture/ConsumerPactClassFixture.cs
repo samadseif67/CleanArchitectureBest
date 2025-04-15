df9d8f65ace5e515a6bbbaffc28bb44235e0c404
samad <samadseif67@gmail.com>
2025-04-15 23:23:57 +0330

    1404-01-27

---
+++ OrderAppConsumerTest_BetweenTwoProject/ClassFixture/ConsumerPactClassFixture.cs
@@ -0,0 +1,61 @@
+﻿using PactNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PactNet.Mocks.MockHttpService;
+namespace OrderAppConsumerTest_BetweenTwoProject.ClassFixture
+{
+    //https://github.com/pact-foundation/pact-workshop-dotnet-core-v1
+    public class ConsumerPactClassFixture : IDisposable
+    {
+        public IPactBuilder PactBuilder { get; private set; }
+        public IMockProviderService MockProviderService { get; private set; }
+
+        public int MockServerPort { get { return 1002; } }
+        public string MockProviderServiceBaseUri { get { return String.Format("http://localhost:{0}", MockServerPort); } }
+
+
+        public ConsumerPactClassFixture()
+        {
+            var pactConfig = new PactConfig
+            {
+                SpecificationVersion = "2.0.0",
+                PactDir = @"c:\pacts",//در این مسیر فایل قرارداد ایجاد میشود
+                LogDir = @".\pact_logs"
+            };
+            PactBuilder = new PactBuilder(pactConfig);//باعث میشود که فایل جی سان در مسیر بالا ساخته شود
+
+            PactBuilder.ServiceConsumer("Consumer")
+                       .HasPactWith("Provider");
+            MockProviderService = PactBuilder.MockService(MockServerPort);
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // This will save the pact file once finished.
+                    PactBuilder.Build();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+        }
+        #endregion
+
+    }
+}
---
+++ OrderAppConsumerTest_BetweenTwoProject/Service/ConsumerPactTests.cs
@@ -0,0 +1,85 @@
+﻿using OrderAppConsumer.Service;
+using OrderAppConsumerTest_BetweenTwoProject.ClassFixture;
+using PactNet.Matchers;
+using PactNet.Mocks.MockHttpService;
+using PactNet.Mocks.MockHttpService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderAppConsumerTest_BetweenTwoProject.Service
+{
+    public class ConsumerPactTests : IClassFixture<ConsumerPactClassFixture>
+    {
+        private IMockProviderService _mockProviderService;
+        private string _mockProviderServiceBaseUri;
+
+        public ConsumerPactTests(ConsumerPactClassFixture fixture)
+        {
+            _mockProviderService = fixture.MockProviderService;
+            _mockProviderService.ClearInteractions(); //NOTE: Clears any previously registered interactions before the test is run
+            _mockProviderServiceBaseUri = fixture.MockProviderServiceBaseUri;
+        }
+
+
+        [Fact]
+        public void VerifyrPoductService_Verify()
+        {
+            // Arange
+          
+            Guid guid = Guid.NewGuid();
+            //کد پایین باعث ایجاد یک سرور میشود 
+            //با مسیر پایین
+            //$"/api/Product/verify/{guid.ToString()}",
+            //و با خروجی پایین که انتظار داریم سرور اطلاعات را برگرداند
+            //Body = Match.Type(new//وقتی از این قسمت استفاده میکنیم یعنی باید ساختار داخلی زیر را داشته باشد 
+                                    //{//حروجی آبجکتی که قراراست وب سرویس به ما برگرداند
+                                     //   id = guid.ToString(),
+                                      //  name = "hello ok"
+                                   // })
+
+
+
+            _mockProviderService.Given("There is correct data")
+                                .UponReceiving("product information return")
+                                .With(new ProviderServiceRequest
+                                {
+                                    Method = HttpVerb.Get,
+                                    Path = $"/api/Product/verify/{guid.ToString()}",
+                                   // Query = " "//query string 
+                                })
+                                .WillRespondWith(new ProviderServiceResponse
+                                {
+                                    Status = 200,
+                                    Headers = new Dictionary<string, object>
+                                    {
+                                { "Content-Type", "application/json; charset=utf-8" }
+                                    },
+                                    Body = Match.Type(new//وقتی از این قسمت استفاده میکنیم یعنی باید ساختار داخلی زیر را داشته باشد 
+                                    {//حروجی آبجکتی که قراراست وب سرویس به ما برگرداند
+                                        id = guid.ToString(),
+                                        name = "hello ok"
+                                    })
+                                });
+
+
+            //Act
+            IVerifyrPoductService VerifyrPoductService = new VerifyrPoductService(new RestSharp.RestClient(_mockProviderServiceBaseUri));
+            var result = VerifyrPoductService.Verify(new ProductDto()
+            {
+                id = guid,
+                Name = "hello ok1"
+            });
+
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.NotNull(result.Name);
+
+        }
+
+
+    }
+}
