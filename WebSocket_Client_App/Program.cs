f79a014c53109a265561120cd9dee11a0133f3a4
samad <samadseif67@gmail.com>
2025-04-25 23:50:22 +0330

    1404-02-05

---
+++ WebSocket_Client_App/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using System.Net.WebSockets;
+using System.Text;
+
+Console.WriteLine("press a enter:");
+Console.ReadLine();
+using (ClientWebSocket client = new ClientWebSocket())
+{
+
+
+    Uri serviceUri = new Uri("ws://localhost:5088/send");
+    var cts = new CancellationTokenSource();
+    cts.CancelAfter(TimeSpan.FromSeconds(120));
+
+    try
+    {
+        await client.ConnectAsync(serviceUri, cts.Token);
+        var n = 0;
+        while (client.State == WebSocketState.Open)
+        {
+            Console.WriteLine("enter a msg to send");
+            string message = Console.ReadLine();
+            if (!string.IsNullOrEmpty(message))
+            {
+                ArraySegment<byte> byteToSend = new ArraySegment<byte>(Encoding.UTF8.GetBytes(message));
+                await client.SendAsync(byteToSend, WebSocketMessageType.Text, true, cts.Token);
+                var responseBuffer = new byte[1024];
+                var offset = 0;
+                var packet = 1024;
+                while (true)
+                {
+                    ArraySegment<byte> byteRecived = new ArraySegment<byte>(responseBuffer, offset, packet);
+                    WebSocketReceiveResult response = await client.ReceiveAsync(byteRecived, cts.Token);
+                    var responseMessage = Encoding.UTF8.GetString(responseBuffer, offset, response.Count);
+                    Console.WriteLine(responseMessage);
+                    if (response.EndOfMessage)
+                    {
+                        break;
+                    }
+                }
+            }
+
+        }
+    }
+    catch (WebSocketException e)
+    {
+
+        Console.WriteLine(e.Message);
+    }
+
+}
+
+Console.ReadLine();
+
