b67d1bc27288a5c40f6761f1b3436d5c2d13ab95
samad <samadseif67@gmail.com>
2025-05-01 15:09:55 +0330

    1404-02-11

---
+++ Docker_App/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Docker_App.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpPost]
+        public ActionResult Test1()
+        {
+            return Ok("hello hello");
+        }
+
+
+
+    }
+}
