ef4b0298d0d431082115821b7710badff8aa4c7d
samad <samadseif67@gmail.com>
2025-05-04 23:14:26 +0330

    1404-02-14 2314

--- Docker_App/Program.cs
+++ Docker_App/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || 1==1)
 {
     app.UseSwagger();
     app.UseSwaggerUI();
