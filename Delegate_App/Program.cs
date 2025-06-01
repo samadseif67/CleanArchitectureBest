28e24b6c1ebb2f4041ccd37d1a10c313e544ce7a
samad <samadseif67@gmail.com>
2025-06-01 23:31:32 +0330

    1404-03-1101

---
+++ Delegate_App/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            //روش اول
+            //delegate
+            //program.Test(program.Sum);
+            //program.Test(program.Differentiation);
+            //program.Test((A, B) => A + B);
+
+
+            //روش دوم
+            //Func
+            //program.Test2(program.Sum);
+
+            //روش سوم
+            //Action
+            program.Test3(program.Nothing);
+
+
+            Console.ReadKey();
+        }
+
+        //*************************************************************************************
+        //روش اول
+        public delegate int SumTest(int A, int B);
+        public void Test(SumTest sumTest)
+        {
+            Console.WriteLine(sumTest(10, 15));
+        }
+        public int Sum(int A, int B)
+        {
+            return A + B;
+        }
+        public int Differentiation(int A, int B)
+        {
+            return A - B;
+        }
+        //***************************************************************************************
+        //روش دوم
+        //در مثال پایین دو تا ورودی صحیح و یک خروجی صحیح برمیگرداند
+        //و تابعی که بعنوان ورودی به این متد ارسال میشود باید دو تا ورودی و یک خروجی داشته باشد
+        public void Test2(Func<int, int, int> func1)
+        {
+            var number = func1(14, 18);
+            Console.WriteLine(number);
+        }
+
+        //***************************************************************************************
+        //روش سوم
+        //در این روش ورودی تابع داریم اما خروجی نداریم
+
+        public void Test3(Action<int, int> action)
+        {
+
+            action(10, 12);
+        }
+
+        public void Nothing(int A, int B)
+        {
+            Console.WriteLine(A + B);
+        }
+
+
+
+        //**************************************************************************************
+     
+
+
+
+
+    }
+}
---
+++ MultiThreading_App/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreading_App
+{
+    public class Program
+    {
+        //مواقعی از مالتی ترید استفاده میکنیم که خط پایین وابستگی به خط بالا نداشته باشد
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            //program.FirstMethod();
+            program.TwoMehtod();
+            Console.ReadKey();
+
+           
+
+        }
+
+        //روش اول
+        public void FirstMethod()
+        {
+            Thread t1 = new Thread(MsgConsole);
+            t1.Start();
+
+            Thread t2 = new Thread(MsgConsole);
+            t2.Start();
+
+
+            Thread t3 = new Thread(MsgConsole);
+            t3.Start();
+
+
+            t1.Join();//ابتدا ترید های بالا انجام شود سپس کد های پایین
+            t2.Join();
+            t3.Join();
+
+
+            Console.WriteLine("Test2");
+        }
+
+        public void MsgConsole()
+        {
+            Thread.Sleep(3000); 
+            Console.WriteLine("Test1");
+        }
+
+
+
+        //*********************************************************
+        //روش دوم
+        public void TwoMehtod()
+        {
+            Task.Run(MsgConsole);
+            Task.Run(MsgConsole);
+            Task.Run(MsgConsole);
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
