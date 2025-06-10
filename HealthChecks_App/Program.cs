e68b57dacf233e1c325555101a0bca395601f3c4
samad <samadseif67@gmail.com>
2025-06-10 23:21:03 +0330

    1404-03-20 1120

--- HealthChecks_App/Program.cs
+++ HealthChecks_App/Program.cs
@@ -1,11 +1,23 @@
+﻿using HealthChecks.UI.Client;
+using HealthChecks.UI.Configuration;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Net;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //***************************************AddHealthChecks*********************************************
+//https://github.com/Xabaril/AspNetCore.Diagnostics.HealthChecks      
 
+builder.Services.AddHealthChecks().AddSqlServer(builder.Configuration.GetConnectionString("DBConnectionStrings"));
 
-builder.Services.AddHealthChecks();
-
-
+// افزودن سرویس HealthChecks UI
+builder.Services.AddHealthChecksUI(options =>
+{
+    options.SetEvaluationTimeInSeconds(60); // زمان ارزیابی سلامت سرویس ها (ثانیه)
+    options.MaximumHistoryEntriesPerEndpoint(50); // حداکثر تعداد لاگ های ذخیره شده
+    options.AddHealthCheckEndpoint("API Health", "/health"); // آدرس endpoint سلامت
+})
+.AddInMemoryStorage(); // برای ذخیره سازی نتایج (در این مثال از حافظه موقت استفاده شده)
 
 //*************************************************************************************
 
@@ -21,19 +33,31 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+//*********************************************AddHealthChecks********************************************
 
+//app.MapHealthChecks("/api/health");
+app.UseHealthChecks("/health", new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions
+{
+    Predicate = _ => true,
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,//باعث میشود ui بهتری ارائه شود
+});
+//برای نمایش ظاهری جی سان
+
+app.UseHealthChecksUI(options =>
+{
+    options.UIPath = "/health-ui"; // مسیر دلخواه برای UI
+    options.ApiPath = "/health-api"; // مسیر API برای UI
+});
+ 
+ 
+//****************************************************************************************
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
-//*********************************************AddHealthChecks********************************************
 
-app.MapHealthChecks("/api/health");
-
-
-//****************************************************************************************
 
 
 app.Run();
