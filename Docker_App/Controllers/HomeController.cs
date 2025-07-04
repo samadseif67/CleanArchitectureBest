08af18bc9ea848768a18d08d1630e2413726efce
samad <samadseif67@gmail.com>
2025-07-04 23:02:06 +0330

    1404-04-12B

--- Docker_App/Controllers/HomeController.cs
+++ Docker_App/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Docker_App.Controllers
 {
@@ -7,17 +8,38 @@ namespace Docker_App.Controllers
     [ApiController]
     public class HomeController : ControllerBase
     {
+        private readonly HttpClient _client;
+        private readonly IConfiguration _configuration;
+        public HomeController(HttpClient client, IConfiguration configuration)
+        {
+            _client = client;
+            _configuration = configuration;
+        }
 
         [HttpPost]
         public ActionResult Test1()
         {
-            return Ok("hello hello Test1");
+            var result = Guid.NewGuid();
+            return Ok(result);
         }
 
         [HttpPost]
-        public ActionResult Test2()
+        public async Task<ActionResult> Test2()
         {
-            return Ok("hello hello Test2");
+            try
+            {
+                var urlApiRandom = _configuration.GetValue<string>("UrlApi:ApiRandom");
+                _client.BaseAddress = new Uri(urlApiRandom);
+                StringContent httpContent = new StringContent("", System.Text.Encoding.UTF8, "application/json");
+                var result = await _client.PostAsync("Home/Test1", httpContent);
+                var guid = result.Content.ReadFromJsonAsync<string>().Result;
+                return Ok(guid);
+            }
+            catch (Exception ex)
+            {
+
+                return Ok(ex.Message);
+            }
         }
 
     }
--- Docker_App/Program.cs
+++ Docker_App/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//*******************************************************************************
+
+builder.Services.AddHttpClient();
+
+builder.Services.AddCors(options => options.AddPolicy("ApiCorsPolicy", builder =>
+{
+    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+}));
+//*****************************************************************************
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +23,12 @@ if (app.Environment.IsDevelopment() || 1==1)
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+//***********************************************************************
+app.UseCors("ApiCorsPolicy");
+
+//**********************************************************************
+
 
 app.UseHttpsRedirection();
 
